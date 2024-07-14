bff543354bde42945faf685be72fac25cf7e5500
Jaival111 <jaivalchauhan111@gmail.com>
2024-07-14 10:55:51 +0530

    Updated Home Page

--- JavaScript/nav-anim.js
+++ JavaScript/nav-anim.js
@@ -1,6 +1,6 @@
 // Menu animation
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mySidenav").style.width = "300px";
 }
 
 function closeNav() {
